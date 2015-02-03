259bef3554a4212c3339a5432dbcd419429f631f
Remco Overdijk <remco@maxserv.com>
2015-02-03 10:18:05 +0100

    Implemented a WordLibrary wrapper that is able to load a library from file into an array. Provided some basic unit tests for loading the lib.

---
+++ src/WordLibrary.php
@@ -0,0 +1,46 @@
+<?php
+
+class WordLibrary
+{
+	/**
+	 * @var Array <String>
+	 */
+	private $wordList;
+
+	public function __construct() {
+
+	}
+
+	public static function withFileName($fileName) {
+		$instance = new self();
+		$instance->loadByFileName($fileName);
+		return $instance;
+	}
+	private function loadByFileName($fn) {
+		if(!file_exists($fn) || !is_file($fn)) throw new ErrorException('Word Library could not be found');
+		$this->wordList = file($fn, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	}
+	public function reduceSetByStringLength($stringLength) {
+		if(!is_int($stringLength)) throw new ErrorException('Cannot recude by a non-numeric value');
+		foreach($this->wordList as $key=>$value) {
+			if(strlen($value) != $stringLength) unset($this->wordList[$key]);
+		}
+	}
+
+	/**
+	 * @return Array
+	 */
+	public function getWordList()
+	{
+		return $this->wordList;
+	}
+
+	/**
+	 * @param Array $wordList
+	 */
+	public function setWordList($wordList)
+	{
+		$this->wordList = $wordList;
+	}
+
+}
---
+++ tests/WordLibraryTest.php
@@ -0,0 +1,25 @@
+<?php
+require_once('src/WordLibrary.php');
+
+class WordLibraryTest extends PHPUnit_Framework_TestCase {
+	public function testCanBeLoadedFromFile() {
+		$fn = '/usr/share/dict/words';
+		$this->assertFileExists($fn);
+
+		$a = WordLibrary::withFileName($fn);
+		$this->assertInstanceOf('WordLibrary', $a);
+		$this->assertGreaterThan(1, count($a->getWordList()));
+	}
+	public function testCannotBeLoadedFromBogusFile() {
+		$fn = '/does/not/exist';
+		$this->assertFileNotExists($fn);
+		$this->setExpectedException('ErrorException', 'Word Library could not be found');
+		$a = WordLibrary::withFileName($fn);
+	}
+	public function testCannotBeLoadedFromDirectory() {
+		$fn = '/usr/local';
+		$this->assertFileExists($fn);
+		$this->setExpectedException('ErrorException', 'Word Library could not be found');
+		$a = WordLibrary::withFileName($fn);
+	}
+}
